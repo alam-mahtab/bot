4d75accfe7b31c40caf2e97e86805e6c7d303e9f
ICSEVANIDHI-4\MY <mahtab.alam@mobirizer.com>
2021-05-22 16:26:21 +0530

    db section removed

--- bot.py
+++ bot.py
@@ -17,8 +17,8 @@ import datetime
 
 bot = telebot.TeleBot(config.TOKEN)
 
-conn = sqlite3.connect('database.db', check_same_thread=False)
-cursor = conn.cursor()
+# conn = sqlite3.connect('database.db', check_same_thread=False)
+# cursor = conn.cursor()
 
 
 # def db_table_val(user_id: int, user_name: str, user_surname: str, username: str):
